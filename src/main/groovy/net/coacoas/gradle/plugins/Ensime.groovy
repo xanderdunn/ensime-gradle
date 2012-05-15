d90dec530c55348c2c86ad1e0ae2d3ecf91e5ca9
Bill Carlson <bill@coacoas.net>
2012-05-14 22:43:54 -0400

    source-roots are now being created properly for Java projects, not just Scala projects.

--- src/main/groovy/net/coacoas/gradle/plugins/Ensime.groovy
+++ src/main/groovy/net/coacoas/gradle/plugins/Ensime.groovy
@@ -12,82 +12,87 @@ import org.gradle.api.logging.Logging
  *
  * &copy; Bill Carlson 2012
  */
-class Ensime implements Plugin<Project> {
-	public static final String TASK_NAME="ensime"
-	public Project project
-	private Logger logger
-	
-	List<String> getClasspath(String configurationName) {
-		logger.debug("Getting classpath from configuration ${configurationName}")
-		project.configurations.each { 
-			logger.debug("Found configuration: ${it}")
-		}
-		return project.configurations.getByName(configurationName).collect{ 
-			logger.debug(it.absolutePath)
-			it.absolutePath 
-		}
-	}
+ class Ensime implements Plugin<Project> {
+ 	public static final String TASK_NAME="ensime"
+ 	public Project project
+ 	private Logger logger
 
-	EnsimeSetting classpathSetting(String setting, String config) {
-		new EnsimeListSetting(keyword: setting, values: getClasspath(config))
-	}
-	
-	List<String> getSourceSets() { 
-		List<String> sets = project.sourceSets?.main.java?.srcDirs.collect{it.absolutePath} +
-		  project.sourceSets?.main.scala?.srcDirs.collect{it.absolutePath} +
-		  project.sourceSets?.main.resources.srcDirs.collect{it.absolutePath} +
-		  project.sourceSets?.test.resources.srcDirs.collect{it.absolutePath} +
-		  project.sourceSets?.test?.java?.srcDirs.collect{it.absolutePath} +
-		  project.sourceSets?.test?.scala?.srcDirs.collect{it.absolutePath}
-	}
+ 	List<String> getClasspath(String configurationName) {
+ 		logger.debug("Getting classpath from configuration ${configurationName}")
+ 		project.configurations.each { 
+ 			logger.debug("Found configuration: ${it}")
+ 		}
+ 		return project.configurations.getByName(configurationName).collect{ 
+ 			logger.debug(it.absolutePath)
+ 			it.absolutePath 
+ 		}
+ 	}
 
-	String settings() {
-		Map<String, String> props = [
-			'module-name': 'name',
-			'version': 'version',
-			'name': 'name',
-			'package': 'groupId',
-			'target': 'buildDir',
-			'root-dir': 'rootDir',
-		]
-		List<EnsimeSetting> settings = props.collect {
-			new EnsimeStringSetting(keyword: it.key, value: project.properties.get(it.value))
-		} +
-		classpathSetting('compile-deps', 'compile') +
-		classpathSetting('runtime-deps', 'runtime') +
-		classpathSetting('test-deps', 'testRuntime') +
-		new EnsimeListSetting(keyword: 'depends-on-modules', values: project.dependsOnProjects.collect { it.name }) +
-		new EnsimeListSetting(keyword: 'source-roots', values: getSourceSets())
-		return settings.collect{it.toSExp()}.join("\n")
-	}
+ 	EnsimeSetting classpathSetting(String setting, String config) {
+ 		new EnsimeListSetting(keyword: setting, values: getClasspath(config))
+ 	}
 
-	void createEnsimeSettings(EnsimeModel config, File outputFile) {
-		outputFile.parentFile.mkdirs()
-		String data = "(:subprojects\n((" +
-				settings() +
-				")))"
-		logger.info(data)
-		outputFile.write(data)
-	}
+ 	List<String> getSourceSets() { 
+ 		List<String> sets = project.sourceSets?.main?.java?.srcDirs.collect{it.absolutePath} +
+ 		project.sourceSets?.main?.resources?.srcDirs.collect{it.absolutePath} +
+ 		project.sourceSets?.test?.resources?.srcDirs.collect{it.absolutePath} +
+ 		project.sourceSets?.test?.java?.srcDirs.collect{it.absolutePath}
 
-	String getEnsimeTargetFile(File outputFile) { 
-		String fileName = (project.extensions.ensime.target ?: project.projectDir.absolutePath) + "/.ensime" 
-		logger.debug("Writing ensime configuration to ${fileName}")
-		return fileName
+ 		if (project.sourceSets.main.hasProperty('scala')) {
+ 			sets += project.sourceSets?.main?.scala?.srcDirs.collect{it.absolutePath} +
+ 			project.sourceSets?.test?.scala?.srcDirs.collect{it.absolutePath}
+ 		}
+
+ 		sets
+ 	}
+
+ 	String settings() {
+ 		Map<String, String> props = [
+ 		'module-name': 'name',
+ 		'version': 'version',
+ 		'name': 'name',
+ 		'package': 'groupId',
+ 		'target': 'buildDir',
+ 		'root-dir': 'rootDir',
+ 		]
+ 		List<EnsimeSetting> settings = props.collect {
+ 			new EnsimeStringSetting(keyword: it.key, value: project.properties.get(it.value))
+ 			} +
+ 			classpathSetting('compile-deps', 'compile') +
+ 			classpathSetting('runtime-deps', 'runtime') +
+ 			classpathSetting('test-deps', 'testRuntime') +
+ 			new EnsimeListSetting(keyword: 'depends-on-modules', values: project.dependsOnProjects.collect { it.name }) +
+ 			new EnsimeListSetting(keyword: 'source-roots', values: getSourceSets())
+ 			return settings.collect{it.toSExp()}.join("\n")
+ 		}
+
+ 		void createEnsimeSettings(EnsimeModel config, File outputFile) {
+ 			outputFile.parentFile.mkdirs()
+ 			String data = "(:subprojects\n((" +
+ 				settings() +
+ 				")))"
+logger.info(data)
+outputFile.write(data)
+}
+
+String getEnsimeTargetFile(File outputFile) { 
+	String fileName = (project.extensions.ensime.target ?: project.projectDir.absolutePath) + "/.ensime" 
+	logger.debug("Writing ensime configuration to ${fileName}")
+	return fileName
+}
+
+@Override
+public void apply(Project project) {
+	this.project = project
+	logger = Logging.getLogger(getClass())
+	project.extensions.create(TASK_NAME, EnsimeModel)
+	project.task(TASK_NAME) << {
+		inputs.file project.buildFile
+		def outputFile = project.file(getEnsimeTargetFile())
+		outputs.file outputFile
+		createEnsimeSettings(project.extensions.ensime, outputFile) 
 	}
-	
-	@Override
-	public void apply(Project project) {
-		this.project = project
-		logger = Logging.getLogger(getClass())
-		project.extensions.create(TASK_NAME, EnsimeModel)
-		project.task(TASK_NAME) << {
-			inputs.file project.buildFile
-			def outputFile = project.file(getEnsimeTargetFile())
-			outputs.file outputFile
-			createEnsimeSettings(project.extensions.ensime, outputFile) 
-		}
-	}
+}
 }
 
 class EnsimeModel {
